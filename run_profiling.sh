fedac20140285ab51c500564d4b0b94fad961aaa
Craig White <gerzytet@gmail.com>
2025-03-20 14:13:40 -0400

    Add sdsl as a git submodule, move profiling results into dedicated folder

--- run_profiling.sh
+++ run_profiling.sh
@@ -2,4 +2,4 @@ set -e
 
 g++ -std=c++17 -O1 -DNDEBUG -I ~/include -L ~/lib main.cpp -o main -lsdsl -ldivsufsort -ldivsufsort64 -pg
 ./main
-gprof main gmon.out > analysis.txt
+gprof main gmon.out > performance_analysis/analysis.txt
